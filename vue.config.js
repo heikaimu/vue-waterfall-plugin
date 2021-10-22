29f804effeef42d4a534654a7abdd2a0ddde71ec
Liu YaoWen <576079353@qq.com>
2021-10-22 17:57:51 +0800

    预览地址修改

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,10 @@
  * @Description: 
  * @Version: 2.0
  * @Author: Yaowen Liu
- * @Date: 2021-10-22 16:57:02
+ * @Date: 2021-10-22 17:30:28
  * @LastEditors: Yaowen Liu
- * @LastEditTime: 2021-10-22 16:57:02
+ * @LastEditTime: 2021-10-22 17:57:26
  */
+module.exports = {
+  publicPath: 'https://heikaimu.github.io/vue-waterfall-plugin/preview/'
+}
